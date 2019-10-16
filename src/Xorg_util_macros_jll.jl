07d8f43277a81056ea3b9daf79dd97ce055298f2
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-16 17:23:24 +0000

    Snapshot of branch master taken on 2019-10-16T13:23:23.231-04:00 from "https://github.com/UnofficialJuliaMirror/Xorg_util_macros_jll.jl-7c09cfe3-afe2-5798-bcc9-d6b7fecfdca5"

---
+++ src/Xorg_util_macros_jll.jl
@@ -0,0 +1,31 @@
+module Xorg_util_macros_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("7c09cfe3-afe2-5798-bcc9-d6b7fecfdca5"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_util_macros", artifacts_toml) for e in artifacts["Xorg_util_macros"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Xorg_util_macros; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Xorg_util_macros_jll
